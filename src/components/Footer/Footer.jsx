007240087975d8eeb608ef3c263138b2bd817ff8
Mashal <zyadomar112@gmail.com>
2024-04-06 01:27:04 +0200

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,11 +24,12 @@ const Footer = () => {
             <p>من السبت الي الخميس</p>
             <p>من الساعة 12 صباحا الي 12 مساء</p>
           </div>
-          <div>
+          <div className="footer_content_map">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d927758.0370464314!2d47.4820571629815!3d24.724997748455824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f03890d489399%3A0xba974d1c98e79fd5!2z2KfZhNix2YrYp9i2INin2YTYs9i52YjYr9mK2Kk!5e0!3m2!1sar!2seg!4v1712357254144!5m2!1sar!2seg"
               width="400"
               height="300"
+              className="map"
               style={{ border: "0", borderRadius: "1rem" }}
               allowfullscreen=""
               loading="lazy"

e78d31dc56b264db26d00b4d020c63160eec44e8
Mashal <zyadomar112@gmail.com>
2024-05-12 15:35:15 +0300

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -30,9 +30,9 @@ const Footer = () => {
           </div>
           <div className="footer_content_map">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d927758.0370464314!2d47.4820571629815!3d24.724997748455824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f03890d489399%3A0xba974d1c98e79fd5!2z2KfZhNix2YrYp9i2INin2YTYs9i52YjYr9mK2Kk!5e0!3m2!1sar!2seg!4v1712357254144!5m2!1sar!2seg"
               width="400"
               height="300"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3608.777112566103!2d55.33935749999999!3d25.2444311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5d0c1a67a9dd%3A0xf132316c89d6a648!2zNjhWUStRUEggLSDYp9mE2YLYsdmH2YjYr-KAjiAtINiv2KjZiiAtINin2YTYpdmF2KfYsdin2Kog2KfZhNi52LHYqNmK2Kkg2KfZhNmF2KrYrdiv2Kk!5e0!3m2!1sar!2seg!4v1715517085304!5m2!1sar!2seg"
               className="map"
               style={{ border: "0", borderRadius: "1rem" }}
               allowfullscreen=""
